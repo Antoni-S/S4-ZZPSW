f92d9e2aabd13f9f95bbc250467a1ed2cc5f4dc0
Antoni-S <asus@student.agh.edu.pl>
2025-10-06 15:32:36 +0200

    c1/2

--- FreeRTOS/Demo/ARM7_LPC2129_Keil_RVDS/main.c
+++ FreeRTOS/Demo/ARM7_LPC2129_Keil_RVDS/main.c
@@ -1,30 +1,11 @@
 #include "FreeRTOS.h"
 #include "task.h"
 #include "led.h"
+#include "semphr.h"
 
 xTaskHandle xMyHandle;
 
-void LedBlink( void *pvParameters ){
-	unsigned char ucFreq = *((unsigned char*)pvParameters);
-	while(1){
-		Led_Toggle(0);
-		vTaskDelay((1000/ucFreq)/2);
-	}
-}
 
-void LedCtrl( void *pvParameters ) {
-	unsigned char ucTaskStatus = 0;
-	while(1) {
-		if(ucTaskStatus) {
-			vTaskSuspend(xMyHandle);
-			ucTaskStatus = 0;
-		} else {
-			vTaskResume(xMyHandle);
-			ucTaskStatus = 1;
-		}
-		vTaskDelay(1000);
-	}
-}
 
 int main( void )
 {
@@ -32,8 +13,8 @@ int main( void )
 	
 	Led_Init();
 	
-	xTaskCreate(LedCtrl, NULL , 100 , &ucBlinkingFreq, 2, NULL);
-	xTaskCreate(LedBlink, NULL , 100 , &ucBlinkingFreq, 2, &xMyHandle);
+	xTaskCreate(, NULL , 100 , &ucBlinkingFreq, 2, NULL);
+	xTaskCreate(, NULL , 100 , &ucBlinkingFreq, 2, &xMyHandle);
 	vTaskStartScheduler();
 	while(1);
 }
