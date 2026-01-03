c989592d024873a23c4e372c5002c37a000eb661
Antoni-S <asus@student.agh.edu.pl>
2026-01-03 16:42:01 +0100

    4.1.2.3

--- FreeRTOS/Demo/ARM7_LPC2129_Keil_RVDS/main.c
+++ FreeRTOS/Demo/ARM7_LPC2129_Keil_RVDS/main.c
@@ -4,6 +4,7 @@
 #include "semphr.h"
 #include "string.h"
 #include "uart.h"
+#include "keyboard.h"
 
 void LettersTx (void *pvParameters){
     while(1){
@@ -13,9 +14,21 @@ void LettersTx (void *pvParameters){
     }
 }
 
+void KeyboardTx (void *pvParameters) {
+	while(1){
+		if(eKeyboardRead() != RELEASED) {
+			Transmiter_SendString("-KEYBOARD-\n");
+			while (eTransmiter_GetStatus()!=FREE){};
+		}
+		vTaskDelay(300);
+	}
+}
+
 int main( void ){
     UART_InitWithInt(300);
+		KeyboardInit();
     xTaskCreate(LettersTx, NULL, 128, NULL, 1, NULL );
+		xTaskCreate(KeyboardTx, NULL, 128, NULL, 1, NULL);
     vTaskStartScheduler();
     while(1);
 }
