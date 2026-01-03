04cca290d56cd85221078e3ad0bcb72cffa9566f
Antoni-S <asus@student.agh.edu.pl>
2026-01-03 17:26:22 +0100

    4.1.2.5

--- FreeRTOS/Demo/ARM7_LPC2129_Keil_RVDS/main.c
+++ FreeRTOS/Demo/ARM7_LPC2129_Keil_RVDS/main.c
@@ -8,34 +8,35 @@
 
 xSemaphoreHandle xSemaphore;
 
+void Rtos_Transmiter_SendString (char cMessage[]) {
+	xSemaphoreTake(xSemaphore, portMAX_DELAY);
+	Transmiter_SendString(cMessage);
+	while (eTransmiter_GetStatus()!=FREE){};
+	xSemaphoreGive(xSemaphore);
+}
+
 void LettersTx (void *pvParameters){
-    while(1){
-				xSemaphoreTake(xSemaphore, portMAX_DELAY);
-				Transmiter_SendString("-ABCDEFGH-\n");
-				while (eTransmiter_GetStatus()!=FREE){};
-				xSemaphoreGive(xSemaphore);
-				vTaskDelay(300);
-    }
+	while(1){
+		Rtos_Transmiter_SendString("-ABCDEFGH-\n");
+		vTaskDelay(300);
+	}
 }
 
 void KeyboardTx (void *pvParameters) {
 	while(1){
 		if(eKeyboardRead() != RELEASED) {
-			xSemaphoreTake(xSemaphore, portMAX_DELAY);
-			Transmiter_SendString("-KEYBOARD-\n");
-			while (eTransmiter_GetStatus()!=FREE){};
-			xSemaphoreGive(xSemaphore);
+			Rtos_Transmiter_SendString("-KEYBOARD-\n");
 		}
 		vTaskDelay(300);
 	}
 }
 
 int main( void ){
-    UART_InitWithInt(300);
-		KeyboardInit();
-		vSemaphoreCreateBinary(xSemaphore);
-    xTaskCreate(LettersTx, NULL, 128, NULL, 1, NULL );
-		xTaskCreate(KeyboardTx, NULL, 128, NULL, 1, NULL);
-    vTaskStartScheduler();
-    while(1);
+	UART_InitWithInt(300);
+	KeyboardInit();
+	vSemaphoreCreateBinary(xSemaphore);
+	xTaskCreate(LettersTx, NULL, 128, NULL, 1, NULL );
+	xTaskCreate(KeyboardTx, NULL, 128, NULL, 1, NULL);
+	vTaskStartScheduler();
+	while(1);
 }
